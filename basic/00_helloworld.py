c6969eb287d5310ed299cf056123df89bb30f7b7
withhong <50668623+withhong@users.noreply.github.com>
2019-05-16 01:41:18 +0900

    Added scripts for 00_helloworld

---
+++ basic/00_helloworld.py
@@ -0,0 +1,15 @@
+# comment : #
+# 10
+# A = 'hello world'
+# A = A + 1
+
+# multi-line comment : ''' '''
+'''
+Author : name
+Date : 2019.5.15
+'''
+
+# print 'Hello World'
+print('Hello World')
+print('Hello', 'World', '!')
+print(100)
